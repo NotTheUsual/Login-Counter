0c54d13ed71d29e46122676202d7d966197fe82c
James Hunter <james.hunter@me.com>
2014-03-09 09:34:19 +0000

    Added root route, and some more authentication checks

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,15 @@
 Given(/^I am on the sign up page$/) do
   visit new_user_registration_path
 end
+
+When(/^I fill out the sign up form$/) do
+  fill_in 'Email', with: 'ecomba@makersacademy.com'
+  fill_in 'Password', with: 'password1'
+  fill_in 'Password confirmation', with: 'password1'
+  click_button 'Sign up'
+end
+
+Then(/^I should be signed up$/) do
+  user = User.first
+  expect(user).not_to be_nil
+end
